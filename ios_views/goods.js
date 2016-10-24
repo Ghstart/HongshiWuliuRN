bb2a2e86fbfb810f097b782350ae284f1bd42d57
龚欢 <gonghuan2020@gmail.com>
2016-10-24 15:50:48 +0800

    货源布局

--- ios_views/goods.js
+++ ios_views/goods.js
@@ -49,8 +49,76 @@ class goods_views extends Component {
           renderRow={(rowData) =>
               <View style = {{height: 255, justifyContent: 'center', alignItems: 'center', marginTop: -20}}>
                 <Image
-                  source={require('./../images/iosbg@2x.png')}
-                />
+                  source={require('./../images/iosbg@2x.png')}>
+
+                  <View style = {{backgroundColor: 'transparent' ,width: width-10, height: 40, flexDirection: 'row'}}>
+
+                    <Text style = {{paddingLeft: 15, paddingTop: 15, fontSize: 12, color: '#333', flex: 1}}>
+                     承运期 2016.10.05-2016.10.31
+                    </Text>
+
+
+                    <View style = {{paddingTop: 15, paddingRight: 15, flexDirection: 'row'}}>
+                      <Image
+                        style={{paddingRight: 15}}
+                        source={require('./../images/eye@2x.png')}
+                      />
+
+                      <Text style = {{paddingLeft: 5, fontSize: 12, color: '#333'}}>
+                       595
+                     </Text>
+
+                    </View>
+
+                  </View>
+
+                  <View style = {{flex:1}}>
+
+                    <View style = {{flexDirection: 'row'}}>
+                      <Image
+                        style = {{marginLeft : 15, marginTop: 15 }}
+                        source={require('./../images/start@2x.png')}
+                      />
+
+                      <View style = {{flexDirection: 'column', alignItems: 'center'}}>
+                        <Text style = {{marginTop: 15, marginLeft: 5, fontSize: 16, color: '#333'}}>临海市</Text>
+
+                        <Text style = {{marginTop: 15, marginLeft: 5, fontSize: 12, color: '#999999'}}>台州市</Text>
+                      </View>
+
+
+                      <View style = {{flexDirection: 'column', alignItems: 'center'}}>
+                        <Image
+                          style={{marginTop: 15, marginLeft: 5}}
+                          source={require('./../images/truck@2x.png')}
+                        />
+
+                          <Text style = {{marginTop: 18, marginLeft: 5, fontSize: 12, color: '#ff6600'}}>临海市</Text>
+                      </View>
+
+
+                      <Image
+                        style = {{marginLeft : 5, marginTop: 15 }}
+                        source={require('./../images/end@2x.png')}
+                      />
+
+                      <View style = {{flexDirection: 'column',alignItems: 'center'}}>
+                          <Text style = {{marginTop: 15, marginLeft: 5, fontSize: 16, color: '#333'}}>兰溪市</Text>
+
+                          <Text style = {{marginTop: 15, marginLeft: 5, fontSize: 12, color: '#999999'}}>金华市</Text>
+                      </View>
+
+
+
+                    </View>
+
+
+                  </View>
+
+
+                </Image>
+
+
               </View>
           }
           />
