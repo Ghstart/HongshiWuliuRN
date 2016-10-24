d447502f0cb1161d2c53bf1a02e3717b13eff1f9
龚欢 <gonghuan2020@gmail.com>
2016-10-24 22:20:41 +0800

    我的页面

--- ios_views/setting.js
+++ ios_views/setting.js
@@ -2,16 +2,132 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  NavigatorIOS
+  NavigatorIOS,
+  Text,
+  ScrollView,
+  Image
 } from 'react-native';
 
+import Util from './../helpers/Util'
+
+const width = Util.size.width;
+const pix   = Util.pixel;
+const viewWidth = (width - 2 * pix) / 3.0
 
 class setting_views extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      countView: [1,2,3,4,5,6,7]
+    }
+
+
+    console.log(this.state.countView);
+  }
+
   render() {
     return (
-      <View>
+      <ScrollView style = {{backgroundColor: '#f5f5f5'}}>
+        <View style = {{backgroundColor: '#f5f5f5'}}>
+          <View style = {{backgroundColor: '#e23f42',height: 201, marginTop: -20}}>
+            <Text>头部视图</Text>
+          </View>
+          <View style = {{flex: 1, flexDirection: 'row', flexWrap:'wrap'}}>
 
-      </View>
+          <View
+            style={         {width:viewWidth,height:viewWidth,backgroundColor:"#fff",marginRight: pix / 2, marginBottom: pix/2, justifyContent: 'center', alignItems: 'center'}}>
+
+            <Image
+              source={require('./../images/identity@2x.png')}
+            />
+
+            <Text style = {{paddingTop: 3}}>
+              实名认证
+            </Text>
+
+            <Image
+              source={require('./../images/un@2x.png')}
+            />
+          </View>
+
+          <View
+            style={         {width:viewWidth,height:viewWidth,backgroundColor:"#fff", marginRight: pix/2, marginLeft: pix/2, marginBottom: pix/2,  justifyContent: 'center', alignItems: 'center'}}>
+
+            <Image
+              source={require('./../images/enterprise@2x.png')}
+            />
+            <Text style = {{paddingTop: 3}}>
+              企业认证
+            </Text>
+
+            <Image
+              source={require('./../images/un@2x.png')}
+            />
+          </View>
+
+          <View
+            style={         {width:viewWidth,height:viewWidth,backgroundColor:"#fff", marginLeft: pix/2, marginBottom: pix/2,  justifyContent: 'center', alignItems: 'center'}}>
+
+            <Image
+              source={require('./../images/drive@2x.png')}
+            />
+            <Text style = {{paddingTop: 3}}>
+              驾照认证
+            </Text>
+
+            <Image
+              source={require('./../images/un@2x.png')}
+            />
+          </View>
+
+          <View
+            style={         {width:viewWidth,height:viewWidth,backgroundColor:"#fff", marginTop: pix/2, marginRight: pix/2, marginBottom: pix/2,  justifyContent: 'center', alignItems: 'center'}}>
+
+            <Image
+              source={require('./../images/purse@2x.png')}
+            />
+            <Text style = {{paddingTop: 5}}>
+              我的钱包
+            </Text>
+          </View>
+
+          <View
+            style={         {width:viewWidth,height:viewWidth,backgroundColor:"#fff",margin:pix/2,  justifyContent: 'center', alignItems: 'center'}}>
+
+            <Image
+              source={require('./../images/my-truck@2x.png')}
+            />
+            <Text style = {{paddingTop: 5}}>
+              我的车辆
+            </Text>
+          </View>
+
+          <View
+            style={         {width:viewWidth,height:viewWidth,backgroundColor:"#fff", marginTop: pix/2, marginLeft: pix/2, marginBottom: pix/2,  justifyContent: 'center', alignItems: 'center'}}>
+
+            <Image
+              source={require('./../images/service@2x.png')}
+            />
+            <Text style = {{paddingTop: 5}}>
+              联系客服
+            </Text>
+          </View>
+
+          <View
+            style={         {width:viewWidth,height:viewWidth,backgroundColor:"#fff",marginTop: pix/2, marginRight: pix/2, marginBottom: pix/2,  justifyContent: 'center', alignItems: 'center'}}>
+
+            <Image
+              source={require('./../images/setting@2x.png')}
+            />
+            <Text style = {{paddingTop: 5}}>
+              设置
+            </Text>
+          </View>
+
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -38,4 +154,4 @@ const styles = StyleSheet.create({
 
 
 
-module.exports = setting
+module.exports = setting_views
